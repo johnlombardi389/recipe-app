9b0b054529063e8fb80be10aaa8a4f1aa7a72408
johnlombardi389 <lombardij38@gmail.com>
2022-12-08 11:26:18 -0500

    Added link for API fetch on popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,8 @@
 function Popular() {
   const getPopular = async () => {
-    const api = await fetch();
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
     const data = await api.json();
   };
 
