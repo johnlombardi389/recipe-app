0b6c5ea1fa8119b1c44a2f6469bfcb114ff02a19
johnlombardi389 <lombardij38@gmail.com>
2022-12-08 11:33:51 -0500

    Imported useState to hold API data

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Popular() {
+  const [popular, setPopular] = useState([]);
+
   const getPopular = async () => {
     const api = await fetch(
       `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
     const data = await api.json();
+    setPopular(data.recipes);
   };
 
   useEffect(() => {
