aae36d42f1cf70f26d0d01f8a0fdc2579278fac5
johnlombardi389 <lombardij38@gmail.com>
2022-12-07 21:01:07 -0500

    Added components and pages to jsx and created fetch function for api

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,9 @@
-import React from "react";
-
 function Popular() {
+  const getPopular = async () => {
+    const api = await fetch();
+    const data = await api.json();
+  };
+
   return (
     <div>
       <h1>Popular</h1>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 // Components
-import Popular from "./components/Popular";
+import Popular from "../components/Popular";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Popular />
     </div>
   );
 }
