37c6c60f547abb4ff9368ca01e98b35514c40b83
johnlombardi389 <lombardij38@gmail.com>
2022-12-09 14:43:53 -0500

    Styled search bar to have search icon and center on page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,16 +6,46 @@ import { FaSearch } from "react-icons/fa";
 
 function Search() {
   return (
-    <StyledForm>
-      <input type="text" />
-    </StyledForm>
+    <StyledSection>
+      <StyledForm>
+        <FaSearch />
+        <input type="text" />
+      </StyledForm>
+      <h1>Hello</h1>
+    </StyledSection>
   );
 }
 
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 const StyledForm = styled.form`
-  margin: 0 20rem;
   position: relative;
-  width: 100%;
+  width: 75%;
+  padding: 5rem 0rem;
+  input {
+    width: 100%;
+    border: none;
+    font-size: 1.5rem;
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+    padding: 1rem 3rem;
+    border: none;
+    border-radius: 1rem;
+    outline: none;
+  }
+  svg {
+    width: 1.2rem;
+    height: 1.2rem;
+    position: absolute;
+    top: 50%;
+    left: 2.5%;
+    transform: translate(-100%, -50%);
+  }
 `;
 
 export default Search;
