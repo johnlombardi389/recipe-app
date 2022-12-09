87df01a284051e34dc660844cb58ba4f638add6c
johnlombardi389 <lombardij38@gmail.com>
2022-12-09 15:41:20 -0500

    Added dunctionality to searched page and styled all elements to display data from user search on page

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,9 +1,61 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// Style
+import styled from "styled-components";
+
 function Searched() {
+  const [searchedRecipes, setSearchedRecipes] = useState([]);
+
+  let params = useParams();
+
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${
+        import.meta.env.VITE_REACT_APP_API_KEY
+      }&query=${name}`
+    );
+    const recipes = await data.json();
+    setSearchedRecipes(recipes.results);
+  };
+
+  useEffect(() => {
+    getSearched(params.search);
+  }, [params.search]);
+
   return (
-    <div>
-      <h1>Searched</h1>
-    </div>
+    <section>
+      <Grid>
+        {searchedRecipes.map((item) => {
+          return (
+            <Card key={item.id}>
+              <img src={item.image} alt={item.title} />
+              <h4>{item.title}</h4>
+            </Card>
+          );
+        })}
+      </Grid>
+    </section>
   );
 }
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Searched;
