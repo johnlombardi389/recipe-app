6c374f5b5a8d2ef17e489da46c18af0bb6b78e34
johnlombardi389 <lombardij38@gmail.com>
2022-12-09 11:36:27 -0500

    Created RecipeDay component and added images to assets folder

---
+++ src/components/RecipeDay.jsx
@@ -0,0 +1,17 @@
+// Style
+import styled from "styled-components";
+
+function RecipeDay() {
+  return (
+    <section>
+      <div className="recipe">
+        <h5>Recipe Title</h5>
+        <p>Recipe Description</p>
+      </div>
+
+      <div className="graphic"></div>
+    </section>
+  );
+}
+
+export default RecipeDay;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import React from "react";
 // Components
 import Popular from "../components/Popular";
+import RecipeDay from "../components/RecipeDay";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
       <Popular />
+      <RecipeDay />
     </div>
   );
 }
