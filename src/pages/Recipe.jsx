ea99e3cdf9b2daa4dc858bf600773f76e517c63a
johnlombardi389 <lombardij38@gmail.com>
2022-12-11 18:25:38 -0500

    Created Recipe component to act as page for any clicked recipe

---
+++ src/pages/Recipe.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+// Style
+import styled from "styled-components";
+
+function Recipe() {
+  return (
+    <div>
+      <h1>Recipe</h1>
+    </div>
+  );
+}
+
+export default Recipe;
