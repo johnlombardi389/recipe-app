4dfa8c3f4a523bfe6694e49d728e217edf008dcf
johnlombardi389 <lombardij38@gmail.com>
2022-12-11 20:52:06 -0500

    Imported Link from react-router-dom to Popular component so all cards link to a recipe page

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 // Style
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
@@ -45,9 +46,11 @@ function Popular() {
             return (
               <SplideSlide key={recipe.id}>
                 <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
+                  <Link to={"/recipe/" + recipe.id}>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  </Link>
                 </Card>
               </SplideSlide>
             );
